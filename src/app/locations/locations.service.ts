61f495a2911231cbea1b75529841be25ea4eafc2
Ken Nguyen <ken@nekken.ca>
2016-11-04 16:21:37 -0400

    travis CI

--- src/app/locations/locations.service.ts
+++ src/app/locations/locations.service.ts
@@ -3,7 +3,7 @@ import { Headers, Http, Response} from "@angular/http";
 
 import 'rxjs/add/operator/toPromise';
 
-import {Location} from "./location";
+import {Location} from "./Location";
 
 @Injectable()
 export class LocationsService{
